9c607b5478152b66d6d91a5ea8dd97e9378c175f
Pyschduck <156922539+Pyschduck@users.noreply.github.com>
2024-10-04 12:55:44 +0530

    Update mega_LSA.ino

--- mega_LSA.ino
+++ mega_LSA.ino
@@ -2,71 +2,71 @@ const float Kp = 0.5;
 const float Ki = 0.0;
 const float Kd = 0.0;
 const int setpoint = 35;    
-const int baseSpeeda = 150;  
-const int baseSpeedb = 150;
-const int maxSpeeda = 255;
-const int maxSpeedb = 255;   
+const int baseSpeed = 200;  
+const int maxSpeed = 255;   
 int P,D;
 static int I = 0;
 const byte rx = 16;    
 const byte tx = 17;    
-const byte serialEn = 4;    
-const byte junctionPulse = 15;   
-const byte dir1 = 25;   
-const byte dir2 = 26;   
-const byte pwm1 = 27;   
-const byte pwm2 = 14;  
+#define serialEn 4    
+#define junctionPulse 3   
+#define inp1 33   
+#define inp2 35
+#define inp3 37
+#define inp4 39
+#define ENA A4   
+#define ENB A5  
 int positionVal =0;
 int error;
 double motorSpeed;
-void handleNode(){
-  switch(nodecount){
-    case 1:
-    forwards();
-    break;
-    case 2:
-    right();
-    break;
-  }
-  nodecount++;
+#define buttonpin 12
+int lastError = 0; 
+void forwards(int rightMotorSpeed,int leftMotorSpeed){
   
+  digitalWrite(inp1,HIGH);
+  digitalWrite(inp2,LOW);
+  digitalWrite(inp3,HIGH);
+  digitalWrite(inp4,LOW);
+  analogWrite(ENA,rightMotorSpeed);
+  analogWrite(ENB,leftMotorSpeed);
 }
-void setup() {
-  pinMode(dir1, OUTPUT);
-  pinMode(dir2, OUTPUT);
-  pinMode(pwm1, OUTPUT);
-  pinMode(pwm2, OUTPUT);
-  pinMode(serialEn,OUTPUT);   
-  pinMode(junctionPulse,INPUT); 
-  Serial2.begin(115200);
-  Serial.begin(115200);
-  digitalWrite(serialEn,HIGH);
-  digitalWrite(pwm1,LOW);
-  digitalWrite(pwm2,LOW);
-
-  digitalWrite(dir1,HIGH);
-  digitalWrite(dir2,LOW);
-
-  Serial.flush();
-  Serial2.flush();
-
+void backwards(){  
+  digitalWrite(inp1,LOW);
+  digitalWrite(inp2,HIGH);
+  digitalWrite(inp3,LOW);
+  digitalWrite(inp4,HIGH);
+  analogWrite(ENA,100);
+  analogWrite(ENB,100);
 }
 
-int lastError = 0;    
+void right(){
+  digitalWrite(inp1,HIGH);
+  digitalWrite(inp2,LOW);
+  digitalWrite(inp3,HIGH);
+  digitalWrite(inp4,LOW);
+  analogWrite(ENA,100);
+  analogWrite(ENB,0);
+}
 
-void loop() {
-  digitalWrite(serialEn,LOW);  
-  if(Serial1.available() ){ 
-  positionVal = Serial1.read();  
-  Serial.println(positionVal);   
-  if(positionVal == 255) {
-    Serial.println("stop");
-    analogWrite(pwm1,0);
-    analogWrite(pwm2,0);
-  }else if(digitalRead(junctionPulse)){
-    Serial.println("junction");
-    handleNode();
-    }else{
+void left(){
+  digitalWrite(inp1,HIGH);
+  digitalWrite(inp2,LOW);
+  digitalWrite(inp3,HIGH);
+  digitalWrite(inp4,LOW);
+  analogWrite(ENA,0);
+  analogWrite(ENB,100);
+}
+void stop(){
+    digitalWrite(inp1,HIGH);
+  digitalWrite(inp2,LOW);
+  digitalWrite(inp3,HIGH);
+  digitalWrite(inp4,LOW);
+  analogWrite(ENA,0);
+  analogWrite(ENB,0);
+  Serial.println("stop");
+}
+void compute_PID(int positionVal)
+{
     error = setpoint - positionVal;  
     P = error;
     I = I + error;
@@ -75,15 +75,15 @@ void loop() {
     lastError = error; 
     // Serial.println(motorSpeed);
 
-    int rightMotorSpeed = baseSpeeda + (int)motorSpeed;
-    int leftMotorSpeed = baseSpeedb - (int)motorSpeed;
+    int rightMotorSpeed = baseSpeed + (int)motorSpeed;
+    int leftMotorSpeed = baseSpeed - (int)motorSpeed;
 
-    if(rightMotorSpeed > maxSpeeda)
+    if(rightMotorSpeed > maxSpeed)
     { 
-      rightMotorSpeed = maxSpeeda;
+      rightMotorSpeed = maxSpeed;
     }
-    if(leftMotorSpeed > maxSpeedb) {
-      leftMotorSpeed = maxSpeedb;
+    if(leftMotorSpeed > maxSpeed) {
+      leftMotorSpeed = maxSpeed;
     }
     if(rightMotorSpeed < 0) {
 
@@ -93,11 +93,49 @@ void loop() {
     {
       leftMotorSpeed = 0;
     }
-    Serial.printf("L %d\n",leftMotorSpeed);
-    Serial.printf("R %d\n",rightMotorSpeed);
-    // analogWrite(pwm1,rightMotorSpeed);
-    // analogWrite(pwm2,leftMotorSpeed);
+    // Serial.println("L %d\n",leftMotorSpeed);
+    // Serial.println("R %d\n",rightMotorSpeed);
+    forwards(rightMotorSpeed,leftMotorSpeed);
   }
+
+
+void setup() {
+  pinMode(inp1, OUTPUT);
+  pinMode(inp2, OUTPUT);
+  pinMode(inp3, OUTPUT);
+  pinMode(inp4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(serialEn,OUTPUT);   
+  pinMode(junctionPulse,INPUT); 
+  Serial2.begin(115200);
+  Serial.begin(115200);
+  digitalWrite(serialEn,HIGH);
+  digitalWrite(ENA,LOW);
+  digitalWrite(ENB,LOW);
+
+  digitalWrite(inp1,HIGH);
+  digitalWrite(inp2,LOW);
+  digitalWrite(inp3,HIGH);
+  digitalWrite(inp4,LOW);
+
+  Serial.flush();
+  Serial2.flush();
+
+}
+
+   
+
+void loop() {
+  digitalWrite(serialEn,LOW);  
+  if(Serial1.available() ){ 
+  positionVal = Serial1.read();  
+  Serial.println(positionVal);   
+  if(positionVal == 255) {
+    stop();
+  }else{
+      compute_PID(positionVal);
   }
     digitalWrite(serialEn,HIGH); 
   }
+}
